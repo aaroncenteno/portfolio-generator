48b978a8232e998b9a9ae6721350bc37a205acd8
ASC8 <asamcent@gmail.com>
2020-10-02 01:52:18 -0700

    create html page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -140,5 +140,12 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            // console.log('Portfolio complete! Checkout index.html to see the output!');
+        });
         console.log(portfolioData);
     });
